d0030d940ba05bfc6c4ba71c43dfe57b4b30ea5e
Anandprasad03 <anandprasadmsb77@gmail.com>
2026-03-10 00:59:27 +0530

    Small backend fix

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const authRoutes = require('./routes/auth.routes')
 const musicRoutes = require('./routes/music.routes')    
 
 const app = express()
+app.use(cors({ origin: true, credentials: true }))
 
 app.use(express.json())
 app.use(cookieParser())
